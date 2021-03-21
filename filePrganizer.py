6b1f2bced8e2400541db033955aec1a64fe1a985
Kanguchi <70295418+Kanguchi@users.noreply.github.com>
2021-03-21 08:26:20 -0400

    Add files via upload

---
+++ filePrganizer.py
@@ -0,0 +1,15 @@
+import time, os, shutil
+
+path = 'Desktop/ModernChem'
+days = 30
+seconds = time.time() - (days * 24 * 60 * 60)
+
+if os.path.extsts(path):
+    ctime = os.stat(path).st_ctime
+    for root_folder, folders, files in os.walk(path):
+        if seconds >= ctime:
+            os.remove(path)
+            break
+else:
+    print('Path Not Exist')
+
